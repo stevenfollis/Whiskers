dd785f9e9b77719a7ebff89a6eb97807872ecf45
Steven Follis <steven_follis@live.com>
2017-06-06 17:19:39 -0400

    Added browser button functionality

--- dialogs/engagement/my/my.js
+++ dialogs/engagement/my/my.js
@@ -58,6 +58,7 @@ module.exports = {
           .subtitle(`Last updated ${moment(engagement.modifiedon).fromNow()}`)
           .text('Select this engagement with the button')
           .buttons([
+            builder.CardAction.openUrl(session, `${process.env.MICROSOFT_RESOURCE_CRM}/main.aspx?etc=10096&extraqs=formid=33679b2b-bfd5-4e84-a63d-13aa63146ebb&pagetype=entityrecord&id={${engagement.ee_projectid}}`, 'Browser'),
             builder.CardAction.imBack(session, 'Status Update', 'Status Update'),
           ]);
       });
--- dialogs/search/search.js
+++ dialogs/search/search.js
@@ -50,7 +50,7 @@ module.exports = {
             .subtitle(`Last updated ${moment(engagement.modifiedon).fromNow()}`)
             .text('Lorem ipsum')
             .buttons([
-              builder.CardAction.imBack(session, 'Browser', 'Browser'),
+              builder.CardAction.openUrl(session, `${process.env.MICROSOFT_RESOURCE_CRM}/main.aspx?etc=10096&extraqs=formid=33679b2b-bfd5-4e84-a63d-13aa63146ebb&pagetype=entityrecord&id={${engagement.ee_projectid}}`, 'Browser'),
             ]);
         });
 
